a327fc4776014d6a74c84618d56348cc025c8a61
Sajith Perera <sajith.perera@gmail.com>
2013-07-12 08:17:25 +0100

    script to delete a well on a given plate

---
+++ bin/delete_well.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/env perl
+use strict;
+use warnings FATAL => 'all';
+
+use LIMS2::Model;
+use Getopt::Long;
+use Try::Tiny;
+use Pod::Usage;
+use Perl6::Slurp;
+
+my $model = LIMS2::Model->new( user => 'webapp', audit_user => $ENV{USER}.'@sanger.ac.uk' );
+
+GetOptions(
+    'help'         => sub { pod2usage( -verbose => 1 ) },
+    'man'          => sub { pod2usage( -verbose => 2 ) },
+    'plate_name=s' => \my $plate,
+    'well_name=s'  => \my $well,
+    'commit'       => \my $commit,
+);
+
+if ( !$well && !$plate ) {
+    die('Must specify and plate and well');
+}
+
+$model->txn_do(
+    sub {
+        try{
+            $model->delete_well( { plate_name => $plate, well_name => $well } );
+            print "deleted well $plate [$well]\n";
+            unless ( $commit ) {
+                print "non-commit mode, rollback\n";
+                $model->txn_rollback;
+            }
+        }
+        catch {
+            print "delete well failed: $_\n";
+            $model->txn_rollback;
+        };
+    }
+);
+
+=head1 NAME
+
+delete_well.pl - delete a LIMS2 well
+
+=head1 SYNOPSIS
+
+  delete_well.pl [options]
+
+      --help            Display a brief help message
+      --man             Display the manual page
+      --plate_name      Name of plate well belongs to
+      --well_name       Name of well to delete
+      --commit          Commit changes, default is to rollback
+
+=head1 DESCRIPTION
+
+Delete a well in LIMS2, must specify plate the well belongs to.
+Well deletion will not work if well has child wells.
+
+=head1 AUTHOR
+
+Sajith Perera
+
+=head1 BUGS
+
+None reported... yet.
+
+=head1 TODO
+
+=cut
