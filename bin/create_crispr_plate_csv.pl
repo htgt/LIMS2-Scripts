eb1d1b2d1be72dff8e573ce8ff55643be34c1909
Alex Hodgkins <ah19@sanger.ac.uk>
2014-07-23 10:33:46 +0100

    Updated create crispr csv

--- bin/create_crispr_plate_csv.pl
+++ bin/create_crispr_plate_csv.pl
@@ -32,6 +32,17 @@ my $model = LIMS2::Model->new( user => 'webapp', audit_user => $ENV{USER}.'@sang
 
         unless ( defined $filehandles{ $filename } ) {
             $filehandles{ $filename } = file( $filename )->openw;
+            print { $filehandles{ $filename } } "well_name,";
+
+            if ( $file =~ /crispr/ ) {
+                print { $filehandles{ $filename } } "crispr_id\n";
+            }
+            elsif ( $file =~ /design/ ) {
+                print { $filehandles{ $filename } } "design_id\n";
+            }
+            else {
+                die "Can't identify header name for $filename";
+            }
         }
 
         return $filehandles{ $filename };
@@ -52,7 +63,9 @@ my @wells = create_wells();
 my @crispr_designs = $model->schema->resultset("CrisprDesign")->search(
     { plated => 0 },
     { prefetch => "crispr_pair" }
-); 
+);
+
+say "Found " . scalar( @crispr_designs ) . " crispr designs";
 
 die "More CrisprDesigns than wells!" if @wells < @crispr_designs;
 
