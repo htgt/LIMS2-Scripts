3fb16ea47341304128e3f602da0878c344ec4335
Sajith Perera <sajith.perera@gmail.com>
2014-10-07 08:27:39 +0100

    Add scripts used in liftover of assembly

---
+++ liftover/fetch_coords.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use LIMS2::Model;
+use IO::File;
+use feature qw( say );
+
+my $model = LIMS2::Model->new( user => 'tasks' );
+
+my $species = $ARGV[0];
+
+my @DATA = (
+    {
+        resultset => 'DesignOligoLocus',
+        join      => { 'design_oligo' => 'design' },
+        base      => 'design',
+        id        => 'design_oligo_id',
+    },
+    {
+        resultset => 'GenotypingPrimersLoci',
+        join      => { 'genotyping_primer' => 'design' }, 
+        base      => 'design',
+        id        => 'genotyping_primer_id',
+    },
+    {
+        resultset => 'CrisprLocus',
+        join      => 'crispr', 
+        base      => 'crispr',
+        id        => 'crispr_id',
+    },
+);
+
+for my $datum ( @DATA ) {
+    say 'Working on: ' . $datum->{resultset};
+
+    my @loci = $model->schema->resultset( $datum->{resultset} )->search(
+        {
+            $datum->{base} . '.species_id' => $species,
+            assembly_id         => 'GRCh37',
+        },
+        {
+            join     => $datum->{join}, 
+            prefetch => 'chr',
+        }
+    );
+
+    print_bed_file(
+        \@loci,
+        $datum->{id},
+        $datum->{resultset},
+    );
+}
+
+say 'Working on: crispr primers'; 
+my @all_crispr_primer_loci = $model->schema->resultset('CrisprPrimersLoci')->search(
+    {},
+    {
+        join => { 'crispr_oligo' => [ 'crispr', 'crispr_pair', 'crispr_group' ] },
+    }
+);
+
+my @crispr_primers;
+for my $cpl ( @all_crispr_primer_loci ) {
+    my $crispr;
+    my $cp = $cpl->crispr_oligo;
+    if ( $cp->crispr_id ) {
+        $crispr = $cp->crispr;
+    }
+    elsif ( $cp->crispr_pair_id ) {
+        $crispr = $cp->crispr_pair->left_crispr;
+    }
+    elsif ( $cp->crispr_group_id ) {
+        $crispr = $cp->crispr_group->crisprs->first;
+    }
+    else {
+        die( 'Crispr primer not linked to crisprs' );
+    }
+
+    push @crispr_primers, $cpl if $crispr->species_id eq $species;
+
+}
+print_bed_file( \@crispr_primers, 'crispr_oligo_id', 'CrisprPrimersLoci' ); 
+
+sub print_bed_file {
+    my ( $loci, $id, $name ) = @_;
+
+    my $fh = IO::File->new( $name . '.bed' , 'w' );
+
+    foreach my $locus ( @{ $loci } ) {
+        say $fh join "\t",
+            "chr" . $locus->chr->name,
+            $locus->chr_start,
+            $locus->chr_end,
+            $locus->$id . ":" . $locus->chr_strand;
+    }
+}
