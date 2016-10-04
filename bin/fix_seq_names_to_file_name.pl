9ec8e77369b8b6ce99a278274c58e1a7315fe96f
Peter Keen <pk8@sanger.ac.uk>
2016-10-04 16:57:28 +0100

    Yaml to design script - hacky attempt

--- bin/fix_seq_names_to_file_name.pl
+++ bin/fix_seq_names_to_file_name.pl
@@ -15,7 +15,7 @@ foreach my $dir_name (@dir_names){
 	    my ($base, $suffix) = ( $old_name =~ /(.*)\.([^\.]*)$/ );
 	    if($suffix eq 'seq'){
 	    	my $content = $data_file->slurp;
-	    	$content =~ s/^> \S*/> $base/g;
+	    	$content =~ s/^>\S*/> $base/g;
 	    	$data_file->spew($content);
 	    }
 	}
